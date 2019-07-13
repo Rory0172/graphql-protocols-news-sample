f5784cab1a53b1552793d1c06c6dcd928a5dd728
Rory Groenewege <rory0172@live.nl>
2019-07-13 13:21:41 +0200

    Added posts protocol to app,

--- app.rb
+++ app.rb
@@ -13,15 +13,28 @@ module Messages
   end
 end
 
-module Contacts
-  def contacts
-    [{name: "Keanu Reeves"}]
+module Posts
+  def posts
+    [
+      {
+        title: "Keanu Reeves", 
+        markdownContent: "Keanu Charles Reeves born September 2, 1964) is a Canadian[a] actor and musician. He gained fame for his starring roles in several blockbuster films, including comedies from the Bill and Ted franchise (1989–2020); action thrillers Point Break (1991), Speed (1994), the John Wick franchise (2014–2021); psychological thriller The Devil's Advocate (1997); supernatural thriller Constantine (2005); and science fiction/action series The Matrix (1999–2003). He has also appeared in dramatic films such as Dangerous Liaisons (1988), My Own Private Idaho (1991), and Little Buddha (1993), as well as the romantic horror Bram Stoker's Dracula (1992).", 
+        link:"google.nl",
+        tags: ["acting", "bikes", "awesome"]
+      },
+      {
+        title: "Jason Momoa",
+        markdownContent: "Joseph Jason Namakaeha Momoa (born August 1, 1979) is an American actor. He played Aquaman in the DC Extended Universe, beginning with the 2016 superhero film Batman v Superman: Dawn of Justice, and in the 2017 ensemble Justice League and his 2018 solo film Aquaman. In Baywatch Hawaii, he portrayed Lifeguard Jason Ioane.[1][2] On television, he played Ronon Dex on the military science fiction television series Stargate Atlantis (2004–2009), Khal Drogo in the HBO fantasy television series Game of Thrones (2011–2019, although he only featured in the first season), and Declan Harp in the CBC series Frontier (2016–present).",
+        link: "https://en.wikipedia.org/wiki/Jason_Momoa",
+        tags: ["acting", "Hawaii", "Got"]
+      }
+    ]
   end
 end
 
 class Query
   include Messages
-  include Contacts
+  include Posts
   def viewer
     nil
   end
